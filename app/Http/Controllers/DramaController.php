c14ebc4a637de695c04f5d68498847572e5f5cee
haruki6419 <haruki6419@outlook.com>
2019-10-23 14:48:09 +0900

    ドラマのモデル、マイグレーションファイル、コントローラーを作成

---
+++ app/Http/Controllers/DramaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Drama;
+use Illuminate\Http\Request;
+
+class DramaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Drama  $drama
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Drama $drama)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Drama  $drama
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Drama $drama)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Drama  $drama
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Drama $drama)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Drama  $drama
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Drama $drama)
+    {
+        //
+    }
+}
