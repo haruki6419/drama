3173cef514bd6c1d0206776b6f84ac88566977a2
haruki6419 <haruki6419@outlook.com>
2019-10-27 18:27:11 +0900

    posts/index.blade.phpを編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,20 @@
-<h1>Posts</h1>
+@extends('layouts.layouts')
 
-@foreach($posts as $post)
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+@section('title', 'lacrud')
 
-    <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
-        <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <button type="submit">Delete</button>
-    </form>
-@endforeach
+@section('content')
+    <h1>Posts</h1>
 
-<a href="/posts/create">New Post</a> 
+    @foreach($posts as $post)
+        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+        <a href="/posts/{{ $post->id }}/edit">Edit</a>
+
+        <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <button type="submit">Delete</button>
+        </form>
+    @endforeach
+
+    <a href="/posts/create">New Post</a>
+@endsection
