39476be69fc9abda741cb465f92ec6a4edd1cac8
haruki6419 <haruki6419@outlook.com>
2019-10-27 18:20:12 +0900

    posts/index.blade.phpを編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,17 @@
 <h1>Posts</h1>
 
+@foreach($posts as $post)
+    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+    <a href="/posts/{{ $post->id }}/edit">Edit</a>
+@endforeach
+
 <a href="/posts/create">New Post</a>
+
+@if (session('message'))
+        {{ session('message') }}
+    @endif
+
+    {{ $post->title }}
+    {{ $post->content }}
+
+    <a href="/posts/{{ $post->id }}/edit">Edit</a>
