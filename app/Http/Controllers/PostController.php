0c4314ce0650543adb27a3be5ad6b91710dcd3e8
haruki6419 <haruki6419@outlook.com>
2019-10-27 18:25:04 +0900

    PostControllerのdestroyを作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -89,6 +89,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+      $post->delete();
+
+      return redirect()->route('posts.index');
     }
 }
