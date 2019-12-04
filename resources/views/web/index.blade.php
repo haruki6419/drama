261fa8c460d7421359dce4c27bc01d7cd445f559
haruki6419 <haruki6419@outlook.com>
2019-12-04 22:02:06 +0900

    jumbotronの追加

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -4,8 +4,15 @@
 
 @section('content')
 
-<h1>ドラマランキング</h1>
-
+<div class="jumbotron">
+  <h1 class="display-4">ようこそ！ドラマランキングへ</h1>
+  <p class="lead">このサイトでは今話題のドラマの感想を気軽に投稿できるサイトです。</p>
+  <hr class="my-4">
+  <p></p>
+  <div class="d-flex justify-content-center">
+    <a class="btn btn-primary btn-lg" href="/posts" role="button">感想をチェック！</a>
+  </div>
+</div>
 
 <ul>
   <a href="../dramas"><li>1位のドラマ</li></a>
