7a7f450e1692ec0b29dff7d28933b82ddbe0dfd8
haruki6419 <haruki6419@outlook.com>
2019-12-04 23:16:10 +0900

    人気ドラマ一覧作成

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Drama;
+use App\Post;
 
 class WebController extends Controller
 {
     public function index()
     {
       $recently_dramas = Drama::orderBy('created_at','DESC')->take(3)->get();
+      $dramas = Drama::all();
+      $popular_dramas = [];
+      foreach ($dramas as $drama) {
+        $scores = Post::where('drama_id', $drama->id)->pluck('score')->toArray();
+        if (count($scores) > 0) {
+          $popular_dramas[] = [
+            "id" => $drama->id,
+            "title" => $drama->title,
+            "content" => $drama->content,
+            "score" => array_sum($scores) / count($scores)
+          ];
+        }
+      }
+      usort($popular_dramas, function ($a, $b) {
+        return $a['score'] > $b['score'] ? -1 : 1;
+      });
+      return view('web.index', compact('recently_dramas', 'popular_dramas'));
 
-
-      return view('web.index', compact('recently_dramas'));
     }
 }
