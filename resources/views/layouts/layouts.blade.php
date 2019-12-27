798452c0b2060efada1b306fdf2e38f0a2cf7bc4
haruki6419 <haruki6419@outlook.com>
2019-12-27 22:39:22 +0900

    assetをmixに変更

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,7 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     </head>
     <body>
@@ -14,7 +14,7 @@
 
         @component('components.footer')
         @endcomponent
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ mix('js/app.js') }}"></script>
         <div id="fb-root"></div>
         <script async defer crossorigin="anonymous" src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v5.0"></script>
     </body>
